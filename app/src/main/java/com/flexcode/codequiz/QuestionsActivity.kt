b0198ed4cf130fef1cb7b4090d5c3a7fb413b806
felix280murimi <felixmurimi280@gmail.com>
2021-03-15 20:02:03 +0300

    displaying the questions and their different answers

--- app/src/main/java/com/flexcode/codequiz/QuestionsActivity.kt
+++ app/src/main/java/com/flexcode/codequiz/QuestionsActivity.kt
@@ -2,6 +2,8 @@ package com.flexcode.codequiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_questions.*
 
 class QuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,21 @@ class QuestionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_questions)
 
         val questionList = Constants.getQuestions()
+        Log.i("Question Size", "${questionList.size}")
+
+        val currentPosition = 1
+        val question: Question? = questionList[currentPosition -1]
+
+        progressBar.progress = currentPosition
+        tvProgress.text = "$currentPosition" + "/" + progressBar.max
+
+        tvQuiz.text = question!!.question
+        ivImage.setImageResource(question.image)
+        tvOptionOne.text = question.OptionOne
+        tvOptionTwo.text = question.OptionTwo
+        tvOptionThree.text = question.OptionThree
+        tvOptionFour.text = question.OptionFour
+
 
     }
 }
